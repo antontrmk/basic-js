a624de42a5a50837a93c1a321cb7deefab691155
antontrmk <antontrmk@gmail.com>
2021-03-26 00:58:21 +0300

    feat: complete cats, team, array

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if (Array.isArray(members)) {
+    let result = members.filter(item => typeof(item) === 'string').map(item => {
+      return item.replace(/ /g,'').substring(0, 1).toUpperCase();
+    }).sort().join('');
+    return result;
+  } else {
+    return false;
+  }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  let array = arr.slice();
+
+  array.forEach((item, index, array) => {
+    if (item == '--double-next' && index != array.length - 1) {
+      array[index] = array[index + 1];
+    }
+
+    if (item == '--discard-next' && index != array.length - 1) {
+      array[index + 1] = '--to-delete';
+    } 
+
+    if (item == '--double-prev' && index != 0) {
+      array[index] = array[index - 1];
+    }
+
+    if (item == '--discard-prev' && index != 0) {
+      array[index - 1] = '--to-delete';
+    }
+
+  });
+
+  return array.filter(item => {
+    if (typeof(item) == 'string') {
+      if (item.substring(0, 2) == '--') {
+        return false;
+      }
+    }
+    return true;
+  });
+ 
 };
+
+// console.log(module.exports([ '--discard-next', true, '--discard-prev', true, 0, '--discard-prev' ]));
