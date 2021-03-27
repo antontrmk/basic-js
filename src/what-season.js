6f8284c68c43aa2d354f72e8ecba3e3a6ce13ea5
antontrmk <antontrmk@gmail.com>
2021-03-27 08:30:50 +0300

    feat: complete season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason(date) {  
+  let season;
+  if (date && Object.prototype.toString.call(date) === '[object Date]') {
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    switch(String(date.getMonth() + 1)) {
+      case '12':
+      case '1':
+      case '2':
+        season = 'winter';
+        break;
+      case '3':
+      case '4':
+      case '5':
+        season = 'spring';
+        break;
+      case '6':
+      case '7':
+      case '8':
+        season = 'summer';
+        break;
+      case '9':
+      case '10': 
+      case '11':
+        season = 'autumn';
+        break;
+    }
+    return season;
+  }
+  else if (typeof(date) === 'undefined') {
+    return 'Unable to determine the time of year!';
+  }
+  else {
+    throw {name : "NotImplementedError", message : "too lazy to implement"};
+  }
 };
+
+//console.log(module.exports(new Date(2019, 11, 22, 23, 45, 11, 500)));
