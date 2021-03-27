be2917d2e1a7a7bc0f4817b2176f43afbe7247fa
antontrmk <antontrmk@gmail.com>
2021-03-27 06:20:55 +0300

    feat: complete repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options = {}) {
+  str = String(str);
+
+  options.repeatTimes = typeof(options.repeatTimes) == 'undefined' ? 1 : options.repeatTimes;
+  options.separator = typeof(options.separator) == 'undefined' ? '+' : options.separator;
+  options.addition = typeof(options.addition) == 'undefined' ? '' : options.addition;
+  options.additionRepeatTimes = typeof(options.additionRepeatTimes) == 'undefined' ? 1 : options.additionRepeatTimes;
+  options.additionSeparator = typeof(options.additionSeparator) == 'undefined' ? '|' : options.additionSeparator;
+
+  let addition = Array(options.additionRepeatTimes).fill(undefined).map(item => String(options.addition)).join(options.additionSeparator);
+  let result = Array(options.repeatTimes).fill(undefined).map(item => str + addition).join(options.separator);
+  return result;
 };
   
+
+console.log(module.exports(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }));
