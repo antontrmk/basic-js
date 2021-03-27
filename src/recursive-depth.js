c5b9cc33b871cbdf6ceafbbc61b0f249c0dbc054
antontrmk <antontrmk@gmail.com>
2021-03-27 03:55:56 +0300

    feat: depth and chain tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    
+    if (Array.isArray(arr)) {
+      arr.push(1);
+      let depth = arr.map(item => {
+        return this.calculateDepth(item);
+      });
+      return 1 + Math.max.apply(null, depth);
+    } else {
+      return 0;
+    }
   }
 };
+
+// const depthCalc = new module.exports();
+// console.log(depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]));
+// //console.log(depthCalc.calculateDepth([]));
+
+// // console.log(Array.isArray([]));
+
+
+
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,36 @@
 const CustomError = require("../extensions/custom-error");
-
+let result = [];
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return result.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    result.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position > 0 && typeof(position) == 'number' && position < result.length + 1) {
+      result.splice(position - 1, 1);
+    } else {
+      result = [];
+      throw {name : "NotImplementedError", message : "too lazy to implement"};
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    result.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let arr = result.slice();
+    result = [];
+    return arr.join('~~');
   }
 };
 
 module.exports = chainMaker;
+
+// console.log(module.exports.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
+// console.log(module.exports.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').removeLink(5).finishChain());
+
+console.log(chainMaker.addLink('8.963').reverseChain().reverseChain().reverseChain().reverseChain().addLink({0: 'first', 1: 'second', 'length': 2}).reverseChain().addLink(3.14).addLink('DEF').reverseChain().finishChain());
