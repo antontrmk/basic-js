ed4f67a95bab81999228c0de01630fa55901bbb9
antontrmk <antontrmk@gmail.com>
2021-03-28 03:25:28 +0300

    feat: complete vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,76 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(direction) {
+    if (direction == true || direction == undefined) {
+      this.direction = 'direct';
+    } else {
+      this.direction = 'reverse';
+    }
+  }
+
+  encrypt(message, key) {
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    message = message.toUpperCase().split('');
+
+    //Create key array same length as message including non-alphabetic characters
+    let newKey = [];
+    let sdvigator = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (/[A-Z]/.test(message[i])) {
+        newKey.push(key[(i - sdvigator) % key.length].toUpperCase());
+      } else {
+        sdvigator += 1;
+        newKey.push(message[i]);
+      }
+    }
+
+    //Create cipher
+    let result = message.map((item, index) => {
+      if (/[A-Z]/.test(item)) {
+        return letters[(letters.indexOf(item) + letters.indexOf(newKey[index])) % 26];
+      } else {
+        return item;
+      }
+    });
+
+    return this.direction == 'direct' ? result.join('') : result.reverse().join('');
+
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(encryptedMessage, key) {
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    encryptedMessage = encryptedMessage.toUpperCase().split('');
+    
+    //Create key array same length as message including non-alphabetic characters
+    let newKey = [];
+    let sdvigator = 0;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (/[A-Z]/.test(encryptedMessage[i])) {
+        newKey.push(key[(i - sdvigator) % key.length].toUpperCase());
+      } else {
+        sdvigator += 1;
+        newKey.push(encryptedMessage[i]);
+      }
+    }
+
+    //Create cipher
+    let result = encryptedMessage.map((item, index) => {
+      if (/[A-Z]/.test(item)) {
+        return letters[(26 - letters.indexOf(newKey[index]) + letters.indexOf(item)) % 26];
+      } else {
+        return item;
+      }
+    });
+
+    return this.direction == 'direct' ? result.join('') : result.reverse().join('');
+
   }
 }
 
 module.exports = VigenereCipheringMachine;
+// let cipre = new VigenereCipheringMachine();
+// console.log(cipre.decrypt('AEIHQX SX DLLU!', 'alphonse'));
+
+
